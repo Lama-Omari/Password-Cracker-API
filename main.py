fca2f081949d2f3f51676a51c3a3fba9f73defb8
Lama-Omari <165219541+Lama-Omari@users.noreply.github.com>
2024-03-27 23:08:18 +0000

    Add steps

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
+@app.get("/<password>")
+async def root(password):
+    # 1. Read passwordList.txt file as list of strings
+    # 2. search for the coming password in the password list from step 1
+    # 3. return the found password's number
     return {"message": "Hi LOLO"}
